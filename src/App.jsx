7f0ef83e12352106ed2a3425ead231462878f723
Jae You <jyou88@gmail.com>
2022-12-10 13:57:51 -0600

    12/10 AM Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 // All imported libraries needed for this component to run
 import './index.css';
+import ReactDOM from 'react-dom/client'
 import AllRoutes from "./components/Routes";
 import { fetchAllPosts } from './api/apiHelper';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [postId, setPostId] = useState(null);
+
+  const handleDelete = async () => {
+    console.log('postIdToDelete: ', postIdToDelete);
+    const response = await fetch(`https://strangers-things.herokuapp.com/api/${postIdToDelete}`, {
+      method: 'DELETE',
+    });
+    const date = await response.json();
+    console.log('data: ', data);
+    if (data) {
+      const newPosts = posts.filter(post => post.id !== postIdToDelete);
+      setPosts(newPosts);
+    }
+  }
+
   useEffect(() => {
     const fetchInitialData = async () => {
       setPosts(await fetchAllPosts());
@@ -14,13 +30,29 @@ function App() {
   }, []);
   console.log(posts)
 
-  return (
-    <div className="container">
-
-    </div>
-
-  );
+  return <>
+    <h1>
+      Posts
+    </h1>
+    {
+      postId
+        ? <Update posts={posts} setPosts={setPosts} postId={postId}
+          setPostId={setPostId} />
+        : <Create posts={posts} setPosts={setPosts} />
+    }
+    {
+      posts.map(post => <div key={post.id}>
+        <h3>{post.title}</h3>
+        <div>{post.body}</div>
+        <button type="button" className="btn btn-outline-primary" onClick={() => setPostId(post.id)}>Edit</button>
+        <button type="button" className="btn btn-outlie-danger" onClick={() => setPostId(post.id)}>Delete</button>
+      </div>)
+    }
+  </>
 }
 
 export default App
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />,
+);
